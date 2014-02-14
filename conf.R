d7e18bde9d26a3ac37f379c96642ac2b0a1e6618
MonicaGonzalez <monica.gonzalez@financial-science.net>
2014-02-14 15:47:19 -0600

    'Conf'

--- conf.R
+++
@@ -1,9 +0,0 @@
-#########################################
-# Parametros de la aplicacion  
-#########################################
-
-# Parametros de la BD esta es la de agrofin
-paramsDB <- list(user="humberto", 
-                 password="hgrDB2012", 
-                 dbname="originacion", 
-                 host="148.243.72.81")
